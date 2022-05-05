a866a198c82a753b9f9f99985026bcb41c00560e
MintPeanut <63995863+MintPeanut@users.noreply.github.com>
2022-05-05 23:06:44 +0900

    프로젝트 생성

---
+++ HHMemories/src/main/java/com/hhhmemories/cloud/HhMemoriesApplication.java
@@ -0,0 +1,13 @@
+package com.hhhmemories.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhMemoriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HhMemoriesApplication.class, args);
+	}
+
+}
---
+++ HHMemories/src/test/java/com/hhhmemories/cloud/HhMemoriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhhmemories.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhMemoriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
