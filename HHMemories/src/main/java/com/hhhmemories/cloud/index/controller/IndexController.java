7c28476f8f4fa10ee8609a48ebda057d559ada23
LG <LG@DESKTOP-L0KNOC4>
2022-05-08 14:47:19 +0900

    Indexcontroller / PubController , html 수정

--- HHMemories/src/main/java/com/hhhmemories/cloud/index/controller/IndexController.java
+++ HHMemories/src/main/java/com/hhhmemories/cloud/index/controller/IndexController.java
@@ -1,4 +1,4 @@
-package com.hhhmemories.cloud.controller;
+package com.hhhmemories.cloud.index.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -6,17 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class hhhController {
+public class IndexController {
 
-	@RequestMapping("/test2")
-	public String home() {
-
-		System.out.println("페이지 출력");
-		return "pub/NewFile";
-
-	}
-	
-	@RequestMapping("/index")
+	@RequestMapping(value = "/index")
 	public String index() throws Exception {
 		return "index";
 	}
