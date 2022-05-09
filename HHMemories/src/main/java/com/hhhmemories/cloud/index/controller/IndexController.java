4956fa52067ed4e47e2c9c747dc36ef310d7e147
LG <LG@192.168.219.162>
2022-05-09 21:29:41 +0900

    log4j2 추가

--- HHMemories/src/main/java/com/hhhmemories/cloud/index/controller/IndexController.java
+++ HHMemories/src/main/java/com/hhhmemories/cloud/index/controller/IndexController.java
@@ -1,15 +1,22 @@
 package com.hhhmemories.cloud.index.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 @Controller
 public class IndexController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
 
 	@RequestMapping(value = "/index")
 	public String index() throws Exception {
+		logger.debug("Hey, Log4j2!");
+		logger.info("Hey, Log4j2!");
+		logger.warn("Hey, Log4j2!");
+		
 		return "index";
 	}
 	
