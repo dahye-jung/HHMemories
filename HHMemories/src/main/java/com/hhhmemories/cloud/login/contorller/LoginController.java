09c7b1a6b935464e8e747958714bb3638de6d6a1
gkdisp <gkdisp@naver.com>
2022-05-13 21:46:24 +0900

    loginController 메소드 정리.ver1

--- HHMemories/src/main/java/com/hhhmemories/cloud/login/contorller/LoginController.java
+++ HHMemories/src/main/java/com/hhhmemories/cloud/login/contorller/LoginController.java
@@ -5,10 +5,15 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.hhhmemories.cloud.index.controller.IndexController;
 import com.hhhmemories.cloud.login.service.LoginService;
 
+/**
+ * @author 
+ * @프로그램 설명 : 로그인 관련 컨트롤러
+ */
 @Controller
 public class LoginController {
 	
@@ -17,10 +22,63 @@ public class LoginController {
 	@Autowired
 	private LoginService loginService;
 	
+	// 로그인 페이지 이동
 	@RequestMapping(value = "/login")
 	public String loginPage() throws Exception{
-		return "login/login";
-		
+		return "login/login";	
 	}
-
+	
+	/**
+	 * @Method 회원가입 기능
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping(value = "/signin")
+	public String signIn() {
+		
+		return "";
+	}
+	
+	/**
+	 * @Method 로그인 기능
+	 * @param memberId(로그인 아이디)
+	 * @param memberPwd(로그인 비밀번호)
+	 * @return ?
+	 */
+	@RequestMapping(value = "/signup")
+	public String signUp(String memberId, String memberPwd) {
+		
+		// 입력한 아이디와 비밀번호를 DB에서 조회(select)
+		// 리턴값이 존재하면
+		// 기록을 위해 로그인 정보 DB에 업데이트(update)
+		// 세션에 로그인 정보 저장
+		// 로그인 자동 유지 시간 30분으로 설정
+		
+		return "";
+	}
+	
+	// 로그아웃 기능
+	@RequestMapping(value = "/logout")
+	public String logOut() {
+		
+		//세션에 등록된 로그인 정보 삭제
+		
+		return "";
+	}
+	
+	// 아이디 찾기 기능
+	@RequestMapping(value = "/findid")
+	public String findUserId() { 
+		
+		return "";
+	}
+	
+	// 비빌번호 찾기 기능
+	@RequestMapping(value = "/findpwd")
+	public String findUserPwd() {
+		
+		return "";
+	}
+	
+	
 }
