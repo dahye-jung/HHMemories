cd74d34f48c681e497ddaa0958018b76c765788e
MintPeanut <63995863+MintPeanut@users.noreply.github.com>
2022-05-29 22:05:22 +0900

    비밀번호 변경 Controller

--- HHMemories/src/main/java/com/hhhmemories/cloud/member/contorller/MemberController.java
+++ HHMemories/src/main/java/com/hhhmemories/cloud/member/contorller/MemberController.java
@@ -1,5 +1,7 @@
 package com.hhhmemories.cloud.member.contorller;
 
+import java.util.Random;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -95,8 +97,40 @@ public class MemberController {
 	
 	// 비빌번호 찾기 기능
 	@RequestMapping(value = "/findpwd")
-	public String findUserPwd() {
+	public String findUserPwd(MemberVO memberVo) {
+		
+		memberVo = memberService.selectMemberInfo(memberVo);
+		
+		String pw = tempPassword(10); // 임시비밀번호 설정
 		
 		return "";
 	}
+	
+	// 임시 비밀번호 작성 함수
+	public static String tempPassword(int leng){
+		int index = 0;
+		char[] charSet = new char[] {
+				'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+				'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+				'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+				'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+				'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+		};	//배열안의 문자 숫자는 원하는대로
+
+		StringBuffer password = new StringBuffer();
+		Random random = new Random();
+
+		for (int i = 0; i < leng ; i++) {
+			double rd = random.nextDouble();
+			index = (int) (charSet.length * rd);
+			
+			password.append(charSet[index]);
+			
+			System.out.println("index::" + index + "	charSet::"+ charSet[index]);
+		}
+		
+		return password.toString(); 
+	    //StringBuffer를 String으로 변환해서 return 하려면 toString()을 사용하면 된다.
+	}	
+
 }
