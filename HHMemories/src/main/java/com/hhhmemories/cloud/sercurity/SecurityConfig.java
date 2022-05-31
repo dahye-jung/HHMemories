0cffea8de914af7f0280601721540b945610e96a
dahae <dahae@DESKTOP-BQ1N2JQ>
2022-05-31 17:24:36 +0900

    폴더명 변경 member > user

--- HHMemories/src/main/java/com/hhhmemories/cloud/sercurity/SecurityConfig.java
+++ HHMemories/src/main/java/com/hhhmemories/cloud/sercurity/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 										.antMatchers("/member/**").hasRole("MEMBER")
 					.and()
 						.formLogin()
-						.loginPage("/member/login")
+						.loginPage("/user/login")
 						.defaultSuccessUrl("/")
 					.and()
 						.logout()
